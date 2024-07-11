1ef4f4db93c20c351ec4317befe752087692e92d
neitoo <samuraa855@gmail.com>
2024-07-11 16:33:05 +0300

    fix code

--- frontend/components/RangeSlider.tsx
+++ frontend/components/RangeSlider.tsx
@@ -1,6 +1,5 @@
 import React,
 {
-  useState,
   useEffect,
   useRef,
   ChangeEvent,
@@ -15,6 +14,7 @@ interface RangeSliderProps {
   step: number;
   priceCap: number;
   char?: string | null;
+  onChange: (min: number, max: number) => void;
 }
 
 const RangeSlider: React.FC<RangeSliderProps> = ({
@@ -25,51 +25,49 @@ const RangeSlider: React.FC<RangeSliderProps> = ({
   step,
   priceCap,
   char,
+  onChange,
 }) => {
   const progressRef = useRef<HTMLDivElement>(null);
-  const [minValue, setMinValue] = useState<number>(initialMin);
-  const [maxValue, setMaxValue] = useState<number>(initialMax);
 
   const handleMin = (e: ChangeEvent<HTMLInputElement>) => {
     const value = parseInt(e.target.value, 10);
-    if (maxValue - minValue >= priceCap && maxValue <= max) {
-      if (value < maxValue) {
-        setMinValue(value);
+    if (initialMax - initialMin >= priceCap && initialMax <= max) {
+      if (value < initialMax) {
+        onChange(value, initialMax);
       }
-    } else if (value < minValue) {
-      setMinValue(value);
+    } else if (value < initialMin) {
+      onChange(value, initialMax);
     }
   };
 
   const handleMax = (e: ChangeEvent<HTMLInputElement>) => {
     const value = parseInt(e.target.value, 10);
-    if (maxValue - minValue >= priceCap && maxValue <= max) {
-      if (value > minValue) {
-        setMaxValue(value);
+    if (initialMax - initialMin >= priceCap && initialMax <= max) {
+      if (value > initialMin) {
+        onChange(initialMin, value);
       }
-    } else if (value > maxValue) {
-      setMaxValue(value);
+    } else if (value > initialMax) {
+      onChange(initialMin, value);
     }
   };
 
   useEffect(() => {
     if (progressRef.current) {
-      progressRef.current.style.left = `${(minValue / max) * step}%`;
-      progressRef.current.style.right = `${step - (maxValue / max) * step}%`;
+      progressRef.current.style.left = `${(initialMin / max) * step}%`;
+      progressRef.current.style.right = `${step - (initialMax / max) * step}%`;
     }
-  }, [minValue, maxValue, max, step]);
+  }, [initialMin, initialMax, max, step]);
 
   return (
     <div className="w-full selector pb-0">
       <div className="flex flex-col">
-
-        <div className="flex justify-between items-center ">
+        <div className="flex justify-between items-center">
           <div className="rounded-md">
             <span>От </span>
             <input
               onChange={handleMin}
               type="text"
-              value={numberWithSpaces(minValue)}
+              value={numberWithSpaces(initialMin)}
               className="w-20 rounded-md pointer-events-none focus:outline-none"
             />
             <span>{char}</span>
@@ -80,7 +78,7 @@ const RangeSlider: React.FC<RangeSliderProps> = ({
             <input
               onChange={handleMax}
               type="text"
-              value={numberWithSpaces(maxValue)}
+              value={numberWithSpaces(initialMax)}
               className="w-20 rounded-md pointer-events-none focus:outline-none"
             />
             <span>{char}</span>
@@ -99,17 +97,16 @@ const RangeSlider: React.FC<RangeSliderProps> = ({
               min={min}
               step={step}
               max={max}
-              value={minValue}
+              value={initialMin}
               className="range-min absolute w-full -top-1 h-1 bg-transparent appearance-none pointer-events-none"
             />
-
             <input
               onChange={handleMax}
               type="range"
               min={min}
               step={step}
               max={max}
-              value={maxValue}
+              value={initialMax}
               className="range-max absolute w-full -top-1 h-1 bg-transparent appearance-none pointer-events-none"
             />
           </div>
